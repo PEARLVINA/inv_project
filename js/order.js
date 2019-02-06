4a521861e921236d75fd9030a42e5b27aa48773b
nicay <40624143+NicoleTalagtag@users.noreply.github.com>
2019-02-06 21:12:30 +0800

    Add files via upload

---
+++ js/order.js
@@ -0,0 +1,107 @@
+$(document).ready(function(){
+	var DOMAIN = "http://localhost/invproject/public_html";
+
+	addNewRow();
+
+	$("#add").click(function(){
+		addNewRow();
+	})
+
+	function addNewRow(){
+		$.ajax({
+			url : DOMAIN+"/includes/process.php",
+			method : "POST",
+			data : {getNewOrderItem:1},
+			success : function(data){
+				$("#invoice_item").append(data);
+				var n = 0;
+				$(".number").each(function(){
+					$(this).html(++n);
+				})
+			}
+		})
+	}
+
+	$("#remove").click(function(){
+		$("#invoice_item").children("tr:last").remove();
+		calculate(0,0);
+	})
+
+	$("#invoice_item").delegate(".pid","change",function(){
+		var pid = $(this).val();
+		var tr = $(this).parent().parent();
+		$(".overlay").show();
+		$.ajax({
+			url : DOMAIN+"/includes/process.php",
+			method : "POST",
+			dataType : "json",
+			data : {getPriceAndQty:1,id:pid},
+			success : function(data){
+				tr.find(".tqty").val(data["product_stock"]);
+				tr.find(".pro_name").val(data["product_name"]);
+				tr.find(".qty").val(1);
+				tr.find(".price").val(data["product_price"]);
+				tr.find(".amt").html( tr.find(".qty").val() * tr.find(".price").val() );
+				calculate(0,0);
+			}
+		})
+	})
+
+	$("#invoice_item").delegate(".qty","keyup",function(){
+		var qty = $(this);
+		var tr = $(this).parent().parent();
+		if (isNaN(qty.val())) {
+			alert("Please enter a valid quantity");
+			qty.val(1);
+		}else{
+			if ((qty.val() - 0) > (tr.find(".tqty").val()-0)) {
+				alert("Sorry ! This much of quantity is not available");
+				aty.val(1);
+			}else{
+				tr.find(".amt").html(qty.val() * tr.find(".price").val());
+				calculate(0,0);
+			}
+		}
+
+	})
+
+	/*Order Accepting*/
+
+	$("#order_form").click(function(){
+
+		var invoice = $("#get_order_data").serialize();
+		if ($("#cust_name").val() === "") {
+			alert("Plaese enter customer name");
+		}else{
+			$.ajax({
+				url : DOMAIN+"/includes/process.php",
+				method : "POST",
+				data : $("#get_order_data").serialize(),
+				success : function(data){
+
+					if (data < 0) {
+						alert(data);
+					}else{
+						$("#get_order_data").trigger("reset");
+
+						if (confirm("Do u want to print invoice ?")) {
+							window.location.href = DOMAIN+"/includes/invoice_bill.php?invoice_no="+data+"&"+invoice;
+						}
+					}
+
+						
+						
+
+					
+
+				}
+			});
+		}
+		
+			
+		
+		
+
+	});
+
+});
